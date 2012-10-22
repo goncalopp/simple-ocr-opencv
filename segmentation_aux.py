6a7dff39f9342b42fa213da79bc92daacb39f91d
goncalopp <goncalopp@pekkala.cp.quorumverita.com>
2012-10-22 06:47:47 +0100

    new _guess_lines

--- segmentation_aux.py
+++ segmentation_aux.py
@@ -42,33 +42,37 @@ def guess_line_starts_ends_and_middles( segments, asfloat=False ):
     l= new_list
     return l if asfloat else map(int, l)
 
-
-
-def _guess_lines( ys, max_lines=50, confidence_minimum=3 ):
+def _guess_lines( ys, max_lines=50, confidence_minimum=0.3 ):
     '''guesses and returns text inter-line distance, number of lines, y_position of first line'''
-    means_list, diffs, deviations=[], [], []
-    start_n= 3
+    compactness_list, means_list, diffs, deviations= [], [], [], []
+    start_n= 1
     for k in range(start_n,max_lines):
-        temp, classified_points, means = cv2.kmeans( data=ys, K=k, bestLabels=None, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_MAX_ITER, 1, 10), attempts=1, flags=cv2.KMEANS_PP_CENTERS)
+        compactness, classified_points, means = cv2.kmeans( data=ys, K=k, bestLabels=None, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_MAX_ITER, 1, 10), attempts=2, flags=cv2.KMEANS_PP_CENTERS)
         means=numpy.sort(means, axis=0)
-        #calculate the center of each cluster. Assuming lines are equally spaced...
-        tmp1=numpy.diff(means, axis=0) #diff will be equal or very similar
+        means_list.append( means )
+        compactness_list.append( compactness )
+        if k<3:
+            tmp1= [1,2,500, 550] #forge data for bad clusters
+        else:
+            #calculate the center of each cluster. Assuming lines are equally spaced...
+            tmp1=numpy.diff(means, axis=0) #diff will be equal or very similar
         tmp2= numpy.std(tmp1)/numpy.mean(means) #so variance is minimal
         tmp3= numpy.sum( (tmp1-numpy.mean(tmp1))**2) #root mean square deviation, more sensitive than std
-        means_list.append( means )
         diffs.append(tmp1)
         deviations.append(tmp3)
+
+    compactness_list= numpy.diff(-1/numpy.array( compactness_list ))
+    deviations= numpy.array( deviations[1:] )
+    compactness_list= (compactness_list-numpy.mean(compactness_list))/numpy.std(compactness_list)
+    deviations= (deviations-numpy.mean(deviations))/numpy.std(deviations)
+    aglomerated_metric= 0.0*compactness_list + 0.9*deviations
     
-    i= deviations.index(min(deviations))
-    number_of_lines=  i+start_n
-    inter_line_distance= numpy.mean(diffs[i])
-    first_line= means_list[i][0][0]
+    i= numpy.argmin(aglomerated_metric)+1
     lines= numpy.array( means_list[i] )
     
     #calculate confidence
-    betterness= numpy.sort(deviations, axis=0)
-    betterness= 1/(betterness[:-1]/betterness[1:]) #how much better is each solution compared to the next best?
-    confidence= ( betterness[0] - numpy.mean(betterness) ) / numpy.std(betterness) #number of stddevs
+    betterness= numpy.sort(aglomerated_metric, axis=0)
+    confidence= ( betterness[1] - betterness[0]) / ( betterness[2] - betterness[1])
     if confidence<confidence_minimum:
         raise Exception("low confidence")
     return lines #still floating points

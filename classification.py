f06883fdbe9da4044baa8b490b6411f98f694ede
RedFantom <redfantom@outlook.com>
2017-03-26 13:07:31 +0200

    Revert "OpenCV 3 support (by heartz)"
    This reverts commit 8d4f7ca82d5f11d13e9ebc6d1d7e6acceaf3dd90.

--- classification.py
+++ classification.py
@@ -43,7 +43,7 @@ class Classifier( object ):
 
 class KNNClassifier( Classifier ):
     def __init__(self, k=1, debug=False):
-        self.knn= cv2.ml.KNearest_create()
+        self.knn= cv2.KNearest()
         self.k=k
         self.debug= debug
 
@@ -53,10 +53,10 @@ class KNNClassifier( Classifier ):
         if CLASS_DATATYPE!=numpy.float32:
             classes= numpy.asarray( classes, dtype=numpy.float32 )
         features, classes= Classifier._filter_unclassified( features, classes )
-        self.knn.train( features,cv2.ml.ROW_SAMPLE, classes )
+        self.knn.train( features, classes )
         
     def classify( self, features):
         if FEATURE_DATATYPE!=numpy.float32:
             features= numpy.asarray( features, dtype=numpy.float32 )
-        retval, result_classes, neigh_resp, dists= self.knn.findNearest(features, k= 1)
+        retval, result_classes, neigh_resp, dists= self.knn.find_nearest(features, k= 1)
         return result_classes

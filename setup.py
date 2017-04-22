efd4e55c68e11efa1440072954384c3365d32e9a
RedFantom <redfantom@outlook.com>
2017-04-22 15:30:44 +0200

    Added initial setup files

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name="simpleocr",
+    packages=["simpleocr"],
+    version="1.0.0",
+    description="A library for simple OCR in Python using OpenCV",
+    author="The simple-ocr-opencv authors",
+    url="https://www.github.com/goncalopp/simple-ocr-opencv",
+    download_url="https://www.github.com/goncalopp/simple-ocr-opencv/archive/1.0.0.tar.gz",
+    keywords=["OCR", "OpenCV"],
+    license="AGPL",
+    classifiers=["Programming Language :: Python :: 2.7",
+                 "Programming Language :: Python :: 2 :: Only",
+                 "License :: OSI Approved :: GNU Affero General Public License v2 or later (AGPLv2+)"]
+)

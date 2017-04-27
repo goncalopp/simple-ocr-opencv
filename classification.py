dee01b15e3ee5402790c46fb1ff5d2d3abda411c
RedFantom <redfantom@outlook.com>
2017-04-27 13:21:53 +0200

    Fix bytes assertion

--- classification.py
+++ classification.py
@@ -15,7 +15,7 @@ def classes_to_numpy(classes):
     import array
     # utf-32 starts with constant ''\xff\xfe\x00\x00', then has little endian 32 bits chars
     # this assumes little endian architecture!
-    assert chr(15).encode('utf-32') == '\xff\xfe\x00\x00\x0f\x00\x00\x00'
+    assert chr(15).encode('utf-32') == b'\xff\xfe\x00\x00\x0f\x00\x00\x00'
     assert array.array("I").itemsize == 4
     int_classes = array.array("I", "".join(classes).encode('utf-32')[4:])
     assert len(int_classes) == len(classes)

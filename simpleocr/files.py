21f9249662435b031eb1eb54bde241315fd43773
RedFantom <redfantom@outlook.com>
2017-06-18 12:37:08 +0200

    Update import of tesseract_utils

--- simpleocr/files.py
+++ simpleocr/files.py
@@ -1,6 +1,6 @@
 import os
 import cv2
-from tesseract_utils import read_boxfile, write_boxfile
+from simpleocr.tesseract_utils import read_boxfile, write_boxfile
 
 IMAGE_EXTENSIONS = ['.png', '.tif', '.jpg', '.jpeg']
 DATA_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data")

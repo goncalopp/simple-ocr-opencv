384120d8d8de7910f46acaa5acbc5c0a9b287b80
RedFantom <redfantom@outlook.com>
2017-06-21 20:58:53 +0200

    Update setup.py for merging

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from setuptools import setup
 setup(
     name="simpleocr",
     packages=["simpleocr"],
-    version="1.0.0",
+    version="0.1.0",
     description="A library for simple OCR in Python using OpenCV",
     author="The simple-ocr-opencv authors",
     url="https://www.github.com/goncalopp/simple-ocr-opencv",
-    download_url="https://www.github.com/goncalopp/simple-ocr-opencv/archive/1.0.0.tar.gz",
+    download_url="https://www.github.com/goncalopp/simple-ocr-opencv/releases",
     keywords=["OCR", "OpenCV"],
     license="AGPL",
     classifiers=["Programming Language :: Python :: 2.7",
                  "Programming Language :: Python :: 3",
                  "License :: OSI Approved :: GNU Affero General Public License v2 or later (AGPLv2+)"],
     include_package_data=True,
-    install_requires=["opencv-python"]
+    install_requires=["six", "pillow"]
 )

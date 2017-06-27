a752c2e523e2f780469cddea39621d73037463a5
RedFantom <redfantom@outlook.com>
2017-06-27 18:04:18 +0200

    Update __init__.py for import messages

--- simpleocr/__init__.py
+++ simpleocr/__init__.py
@@ -1,3 +1,44 @@
+try:
+    import cv2
+except ImportError as e:
+    import sys
+
+
+    def is_python_3():
+        return sys.version_info[0] is 3
+
+    # Valid values for sys.platform on Linux include "linux" and "linux2"
+    if "linux" in sys.platform:
+        if is_python_3():
+            print(
+                "OpenCV-Python could not be imported. As your are running Linux and Python 3, you have the following "
+                "options to install it:"
+                "\n- Compile OpenCV-Python yourself"
+                "\n- Install the unofficial \"opencv-python\" package from PyPI using pip"
+            )
+        else:
+            print(
+                "OpenCV-Python could not be imported. As you are running Linux and Python 2, you have the following "
+                "options to install it:"
+                "\n- Compile OpenCV-Python yourself"
+                "\n- Install the \"python-opencv\" package with your distro's package manager if it is available"
+                "\n- Install the unofficial \"opencv-python\" package from PyPI using pip"
+            )
+    # The only valid value for Windows is "win32"
+    elif sys.platform is "win32":
+        print(
+            "OpenCV-Python could not be imported. As you are running Windows, you have the following options to "
+            "install it:"
+            "\n- Compile OpenCV-Python yourself"
+            "\n- Install the unofficial \"opencv-python\" package from PyPI using pip"
+        )
+    else:
+        print(
+            "OpenCV-Python could not be imported, but there are no installation instructions available for your OS."
+        )
+    raise e
+
+
 # Classifiers
 from simpleocr.classification import KNNClassifier
 # Files
@@ -12,4 +53,3 @@ from simpleocr.ocr import reconstruct_chars, show_differences, OCR
 from simpleocr.segmentation import RawContourSegmenter, ContourSegmenter
 # Extraction
 from simpleocr.feature_extraction import FeatureExtractor, SimpleFeatureExtractor
-

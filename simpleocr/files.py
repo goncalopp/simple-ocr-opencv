52bfbbcf55d46e157bbc6d46ee4d568c2f42f336
goncalopp <goncalopp@users.noreply.github.com>
2018-03-07 21:32:08 +0000

    fixes

--- simpleocr/files.py
+++ simpleocr/files.py
@@ -10,7 +10,7 @@ GROUND_EXTENSIONS_DEFAULT = GROUND_EXTENSIONS[0]
 
 
 def try_extensions(extensions, path):
-    """ Checks for various extensions if a path exist if the extension is appended """
+    """Checks for various extensions of a path exist if the extension is appended"""
     for ext in [""] + extensions:
         if os.path.exists(path + ext):
             return path + ext
@@ -22,12 +22,10 @@ def open_image(path):
 
 
 def get_file_path(path, ground=False):
-    """
-    Get the absolute path for an image or ground file. Valid inputs are:
-    - Relative path with or without extension
-    - File in DATA_DIRECTORY with or without extension
-    - Absolute path with or without extension
-    :param path: image path in str type
+    """Get the absolute path for an image or ground file.
+    The path can be either absolute, relative to the CWD or relative to the
+    DATA_DIRECTORY. The file extension may be omitted.
+    :param path: image path (str)
     :param ground: whether the file must be a ground file
     :return: The absolute path to the file requested
     """
@@ -43,55 +41,48 @@ def get_file_path(path, ground=False):
     image_datadir = try_extensions(extensions, os.path.join(DATA_DIRECTORY, path))
     if image_datadir:
         return os.path.abspath(image_datadir)
-    # The file cannot be found, so raise a FileNotFound Error
-    raise IOError
+    raise IOError # file not found
 
 
 class Ground(object):
-    """
-    Data class that includes labeled characters of an Image and their positions
-    """
-    def __init__(self, segments=None, classes=None):
+    """Data class that includes labeled characters of an Image and their positions"""
+    def __init__(self, segments, classes):
         self.segments = segments
         self.classes = classes
 
 
 class GroundFile(Ground):
-    """
-    Child class of GroundBuffer with file support. This class can write the data
+    """Ground with file support. This class can write the data
     to a box file so it can be restored when the image file the ground data belongs
     to is opened again.
     """
-    def __init__(self, path, segments=None, classes=None):
-        Ground.__init__(self, segments=segments, classes=classes)
+    def __init__(self, path, segments, classes):
+        Ground.__init__(self, segments, classes)
         self.path = path
 
     def read(self):
-        """ Update the ground data stored by reading the box file from disk """
+        """Update the ground data stored by reading the box file from disk"""
         self.classes, self.segments = read_boxfile(self.path)
 
     def write(self):
-        """ Write a new box file to disk containing the stored ground data """
+        """Write a new box file to disk containing the stored ground data"""
         write_boxfile(self.path, self.classes, self.segments)
 
 
 class Image(object):
-    """
-    Class to work with an image in memory
-    """
+    """An image stored in memory. It optionally contains a Ground"""
     def __init__(self, array):
-        """
-        :param array: array with image data, must be OpenCV compatible
+        """:param array: array with image data, must be OpenCV compatible
         """
         self._image = array
         self._ground = None
 
     def set_ground(self, segments, classes):
-        """ Creates the ground data in memory """
+        """Creates the ground data"""
         self._ground = Ground(segments=segments, classes=classes)
 
     def remove_ground(self):
-        """ Removes the grounding data in memory for the Image """
+        """Removes the grounding data for the Image"""
         self._ground = None
 
     # These properties prevent the user from altering the attributes stored within
@@ -126,20 +117,20 @@ class ImageFile(Image):
         basepath = os.path.splitext(path)[0]
         self._ground_path = try_extensions(GROUND_EXTENSIONS, basepath)
         if self._ground_path:
-            self._ground = GroundFile(self._ground_path)
+            self._ground = GroundFile(self._ground_path, None, None)
             self._ground.read()
         else:
             self._ground_path = basepath + GROUND_EXTENSIONS_DEFAULT
             self._ground = None
 
     def set_ground(self, segments, classes, write_file=False):
-        """ creates the ground, saves it to a file """
+        """Creates the ground, saves it to a file"""
         self._ground = GroundFile(self._ground_path, segments=segments, classes=classes)
         if write_file:
             self.ground.write()
 
     def remove_ground(self, remove_file=False):
-        """ Removes ground, optionally deleting it's file """
+        """Removes ground, optionally deleting it's file"""
         self._ground = None
         if remove_file:
             os.remove(self._ground_path)
--- simpleocr/pillow_utils.py
+++ simpleocr/pillow_utils.py
@@ -5,8 +5,7 @@ import cv2
 
 
 def image_to_pil(imagefile):
-    """
-    Convert an ImageFile or ImageBuffer object to a Pillow Image object
+    """Convert an ImageFile or ImageBuffer object to a Pillow Image object
     :param imagefile: ImageFile object
     :return: Image object
     """
@@ -15,13 +14,11 @@ def image_to_pil(imagefile):
 
 
 def pil_to_image(pillow):
-    """
-    Convert a Pillow Image object to an ImageBuffer object
-    """
+    """Convert a Pillow Image object to an ImageBuffer object"""
     return Image.fromarray(pil_to_cv_array(pillow))
 
 
 def pil_to_cv_array(pillow):
-    """ Convert a Pillow Image object to a cv compatible array """
+    """Convert a Pillow Image object to a cv compatible array"""
     imagefile = numpy.array(pillow)
     return imagefile[:, :, ::-1].copy()
